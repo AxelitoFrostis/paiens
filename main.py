3d698436bf8fcf11f659de72bb3ea930155153fd
AxelitoFrostis <69399407+AxelitoFrostis@users.noreply.github.com>
2021-04-01 08:46:01 +0200

    some more work. Not working yet

--- main.py
+++ main.py
@@ -8,31 +8,29 @@ deck = []
 
 
 
-
-
-
 class card:
     colorDef = ["Spader", "Klöver", "Hjärter", "Ruter"]
     typeDef = ["Ess", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Kneckt", "Dam", "Kung"]
     
-    def __init__(self, cardType,cardColor):
+    def __init__(self, cardType,cardColor, visible):
         self.cardType = int(cardType)
         self.cardColor = int(cardColor)
+        self.visible = bool(visible)
 
     def __repr__(self):
-        return(f"{card.colorDef[self.cardColor]} {card.typeDef[self.cardType]}")
+        return(f"{card.colorDef[self.cardColor]} {card.typeDef[self.cardType]} ({self.visible})")
     
     @classmethod
     def resetDeck(cls):
         cls.deck = []
         for x in range(4):
             for y in range (13):
-                cls.deck.append(card(y,x))
+                cls.deck.append(card(y, x, False))
 
     @classmethod
-    def genCard(cls):
-        cardOut = r.choise(cls.deck)
-        deck.remove(cardOut)
+    def genCard(cls, visible):
+        cardOut = cls.deck.pop(r.randint(0,len(cls.deck)-1))
+        cardOut.visible = bool(visible)
         return(cardOut)
 
 class table:
@@ -41,19 +39,63 @@ class table:
         self.generate()
 
     def generate(self):
-        self.table = np.zeros((SIZE, SIZE))
-        for y in range (SIZE):
-            for x in range (SIZE):
-                if x + y <= SIZE-1:
+        card.resetDeck()
+        self.table = np.empty(shape = (self.SIZE * 2, self.SIZE), dtype=object)
+        for y in range (self.SIZE):
+            for x in range (self.SIZE):
+                if x + y <= self.SIZE-1:
+                    self.table[y,x] = card.genCard(x + y == self.SIZE-1)
+        self.deck = card.deck
+
+
+class run:
+    def __init__(self, level, table):
+        self.level = int(level)
+        self.table = table
+
+        if self.level == 0:
+            self.easy(self.table)
+        elif self.level == 1:
+            self.hard(self.table)
+    
+    def easy(self, table):
+        run.match(table, run.drawCard.easy())
+
+    def hard(self, table):
+        run.match(table, run.drawCard.hard())
+
+    @staticmethod
+    def match(table, card):
+        cols = [i for i in range (table.SIZE)]
+        compare = [card]
+            
+        for x in cols:
+            for y in range (table.SIZE * 2 - 1):
+                try:
+                    if type(table.table [y, x]) == type(card):
+                        if table.table [y, x].visible ==  True:
+                            compare.append(table[y, x])
+                except:
                     pass
-                else:
-                    pass
+
+        print(compare)
+        
+    
+    class drawCard:
+        @staticmethod
+        def easy():
+            return
+
+        @staticmethod
+        def hard():
+            return
 
 
-card.resetDeck()
 
+#pprint (card.deck)
+#print(len(card.deck))
 
+myTable = table()
 
-pprint (card.deck)
-print(len(card.deck))
-
+#pprint(myTable.table)
+instance = run(0, myTable)
